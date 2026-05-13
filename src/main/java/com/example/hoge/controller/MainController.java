2800aa776f22e755e2811d2828f3dbcc70dae0f3
Takahiro Saito <isa05@knightfoundation.onmicrosoft.com>
2026-05-13 09:49:48 +0900

    scaling-testを追加

--- src/main/java/com/example/hoge/controller/MainController.java
+++ src/main/java/com/example/hoge/controller/MainController.java
@@ -1,5 +1,8 @@
 package com.example.hoge.controller;
 
+import java.time.LocalDateTime;
+import java.time.Duration;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,17 @@ public class MainController {
 	@ResponseBody
 	public String index() {
 		return "Hello, MainController! I`m running.";
+	}
+
+	@GetMapping("scaling-test")
+	@ResponseBody
+	public String scalingTest() {
+		LocalDateTime start = LocalDateTime.now();
+		int hoge = 1;
+        while (Duration.between(start, LocalDateTime.now()).getSeconds() < 1) {
+            hoge *= 1;
+        }
+		return "Scaling test completed.\n";
 	}
 
 }
